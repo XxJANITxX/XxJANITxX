67f25826f1de7b234ec7a6b1b140084bda9fb083
Janit <janitgarg12345@gmail.com>
2023-11-10 16:10:40 +0530

    Video 45
    * /backend/index.js - Added express.json() middleware to extract requested data
    
    * /backend/models/Notes.js and /backend/models/User.js - Added Schema Class from mongoose
    
    * /backend/package.json and /backend/package-lock.json - Added express validator through npm
    
    * /backend/routes/auth.js - Imported User schema, created a user from that schema and req.body, saved user in database, responded with "hello"

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ connectToMongo();
 //  Express app
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 // Available routes
 app.use('/api/auth', require('./routes/auth'));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res)=>{
-    
-    obj = {
-        a:'thios',
-        number: 34
-    }
-    
-    res.json(obj);
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save(); 
+    res.send("Hello"); 
 })
 
 module.exports = router;
