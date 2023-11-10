52e73122f584577791ebdff3ff5d4feda2b35310
Janit <janitgarg12345@gmail.com>
2023-11-10 20:52:01 +0530

    Video 46
    * /backend/models/auth.js - Added Express Validator to the project, added validating array to validate each element column of array
    
    * Then, we create User using User.create() functionby passing name, email, password. This returns a promise which can be displayed on the display.
    
    * /backend/models/User.js - Also, we added createIndex().

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const router = express.Router();
 const User = require('../models/User');
+const router = express.Router();
+const {body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save(); 
-    res.send("Hello"); 
+router.post('/', [
+    body('name',"Enter a valid name").isLength({min: 3}),
+    body('email', "Enter a valid Email").isEmail(),
+    body('password', "Enter a valid password").isLength({min: 8})
+],(req, res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({error: errors.array()});
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch((err)=>{
+        console.log(err);
+        res.json({error: 'Please enter a unique value for email'})
+    })
+
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save(); 
+    // res.send(req.body); 
+
 })
 
 module.exports = router;
