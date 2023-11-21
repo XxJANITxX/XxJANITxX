600e778616ecbdd01a0e14dedd27b05745403647
Janit <janitgarg12345@gmail.com>
2023-11-21 19:21:53 +0530

    Video 49
    * backend\package-lock.json ,  backend\package.json - Added jsonwebtoken module and bcrypt module
    
    * backend\routes\auth.js -
    - Imported jsonWebToken and bCrypt modules
    - Created JWT_SECRET string
    - Created salt string using genSalt() of bcrypt
    - Converted passed password and salt to hashstring using bcrypt hash function
    - Passed secPass (hashString) to user database
    - Created data object using containing user object containing user id.
    - Created authorization token using jwt.sign() & JWT_Secret
    - Respoded with created token
    - Displayed error message
    - Added Comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,15 +7,23 @@ const User = require('../models/User');
 const router = express.Router();
 // Using body, validationResult to validate inputs from the user
 const { body, validationResult } = require('express-validator');
+// Importing bcryptjs
+const bcrypt = require('bcryptjs');
+// Importing JWT secret
+const jwt = require('jsonwebtoken');
+
+// Creating JWT secret String
+const JWT_SECRET = "thisIstoMyAuthenticatedUser";
 
 
+// Handling request
 router.post('/createuser', [
     body('name', "Enter a valid name").isLength({ min: 3 }),
     body('email', "Enter a valid Email").isEmail(),
     body('password', "Enter a valid password").isLength({ min: 8 })
 ], async (req, res) => {
 
-    // If error, retun bad request
+    // If error, return bad request
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -25,22 +33,43 @@ router.post('/createuser', [
     // Using create method to create user through manual mapping. Also checks whether user with same email exists or not
 
     try {
+
+        // Checking whether user exists or not
         let user = await User.findOne({ email: req.body.email });
 
         if (user) {
             return res.status(400).json({ error: "Sorry, the user already exists" });
         }
 
+        // Using bcrypt to create salt and converting password to hashString
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+
+        // Creating user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
 
-        res.json(user);
+        // Data object which will send user id as data
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        // Authorization token created using jwt
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        // Responding with authtoken
+        res.json({authtoken});
+
     } catch (error) {
+
+        // Handling error
         console.log("Some error occured");
-        res.status(500).send("Some error occured");
+        res.status(500).send("Some error occured" + "\n"+ error);
     }
 
 })
